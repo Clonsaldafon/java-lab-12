1860f9ed64455071c0b3d03f958f4104b6d7e820
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2024-05-26 20:24:37 +0500

    Initial commit

---
+++ src/main/java/com/example/java_lab_9/JavaLab9Application.java
@@ -0,0 +1,13 @@
+package com.example.java_lab_9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLab9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLab9Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_lab_9/JavaLab9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_lab_9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLab9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
